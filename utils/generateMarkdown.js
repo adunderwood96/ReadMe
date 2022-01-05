cb8c4b2f01e1c1d03a2bc7a4d0a1246438f4ed33
Ashley <adunderwood96@gmail.com>
2022-01-04 19:30:15 -0500

    license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,3 +17,24 @@ function renderLicenseBadge(license) {
     }
   }
   
+  // Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    switch (license) {
+      case "Apache":
+        return "https://opensource.org/licenses/Apache-2.0";
+      case "Eclipse":
+        return "https://opensource.org/licenses/EPL-1.0";
+      case "GNU":
+        return "http://www.gnu.org/licenses/gpl-3.0";
+      case "IBM":
+        return "https://opensource.org/licenses/IPL-1.0";
+      case "MIT":
+        return "https://opensource.org/licenses/MIT";
+      case "Mozilla":
+        return "https://opensource.org/licenses/MPL-2.0";
+      case "None":
+        return;
+    }
+  }
+  
